2b110cf291ba8e36d77f44c3b1c7bc6fb60f1df3
nettee <xf0718@gmail.com>
2015-12-17 21:14:37 +0800

    interface for Trigger and Action

--- src/model/task/Action.java
+++ src/model/task/Action.java
@@ -1,9 +1,11 @@
 package model.task;
 
 import java.io.Serializable;
+import java.util.Map;
 
 public interface Action extends Serializable {
-	
+
 	void perform();
 
+	Map<String, Object> getProperties();
 }
