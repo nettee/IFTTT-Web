1d02aad062ad4029985a9cab9780b94f46957c41
William <nettee.liu@gmail.com>
2016-11-05 16:21:06 +0800

    try slf4j

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import model.data.User;
-
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import database.UserDao;
+import model.data.User;
 
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet {
 
-	private static final Logger logger = Logger.getLogger(LoginServlet.class);
+	private static final Logger logger = LoggerFactory.getLogger(LoginServlet.class);
 
 	@Override
 	public void init() throws ServletException {
@@ -27,42 +27,23 @@ public class LoginServlet extends HttpServlet {
 	public void destroy() {
 	}
 
-	/**
-	 * The doPost method of the servlet. <br>
-	 * 
-	 * This method is called when a form has its tag value method equals to
-	 * post.
-	 * 
-	 * @param request
-	 *            the request send by the client to the server
-	 * @param response
-	 *            the response send by the server to the client
-	 * @throws ServletException
-	 *             if an error occurred
-	 * @throws IOException
-	 *             if an error occurred
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 
 		if (UserDao.confirmPassword(username, password)) {
-			logger.info(String.format("logged in, username=%s, password=%s",
-					username, password));
+			logger.info("logged in, username={}, password={}", username, password);
 
 			HttpSession session = request.getSession();
 			User user = UserDao.getUserByName(username);
 			session.setAttribute("userId", user.getId());
 
-			String userHomePage = getServletContext().getInitParameter(
-					"user home page");
+			String userHomePage = getServletContext().getInitParameter("user home page");
 			response.sendRedirect(userHomePage);
 		} else {
 			logger.info("cannot log in, invalid username or password");
-			String loginPage = getServletContext().getInitParameter(
-					"login page");
+			String loginPage = getServletContext().getInitParameter("login page");
 			String loginPage_Invalid = loginPage + "?valid=no";
 			response.sendRedirect(loginPage_Invalid);
 		}
